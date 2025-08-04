7018fdbdf7f578ab5a52806790a281f3b36ab7e6
AmulyaVegesna <amulya.vrkraju@gmail.com>
2025-08-04 10:17:32 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (40.47%) - LeetSync

---
+++ 3398-make-a-square-with-the-same-color/make-a-square-with-the-same-color.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canMakeSquare(char[][] grid) {
+        for(int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                int w = 0; 
+                int b = 0;
+                if (grid[i][j] == 'W') w++; else b++;
+                if (grid[i][j+1] == 'W') w++; else b++;
+                if (grid[i+1][j] == 'W') w++; else b++;
+                if (grid[i+1][j+1] == 'W') w++; else b++;
+
+                if(Math.max(w,b) >= 3) return true;
+            }
+        }
+        return false;
+    }
+}
