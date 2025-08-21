b0b6c62023ba4962150d29129fd0a87218fd78bb
AmulyaVegesna <amulya.vrkraju@gmail.com>
2025-08-21 14:16:49 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (51.12%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+        String prefix = strs[0];
+        for (String word : strs) {
+            while (word.indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        }   
+        return prefix;
+    }
+}
