5dbada4068ac9ae8ccf601c97faa37e28b03be47
AmulyaVegesna <amulya.vrkraju@gmail.com>
2025-09-11 15:41:51 +0530

    Time: 2914 ms (5.19%) | Memory: 20.1 MB (81.82%) - LeetSync

---
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.py
@@ -0,0 +1,16 @@
+class Solution:
+    def sortVowels(self, s: str) -> str:
+        vowel = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+        l = []
+        for i in range(len(s)):
+            if s[i] in vowel:
+                l.append(s[i])
+        l.sort()
+        result = []
+        for c in s:
+            if c in vowel:
+                result.append(l.pop(0))  # take smallest vowel
+            else:
+                result.append(c)
+    
+        return ''.join(result)
